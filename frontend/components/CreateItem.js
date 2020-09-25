7665ffd1767bbd0fb84031781fe605116f29af36
Jorch <jorge.a.romera@gmail.com>
2020-09-25 18:28:24 -0300

    wip

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -31,6 +31,18 @@ const CreateItem = () => {
     const [image, setImage] = useState('');
     const [largeImage, setLargeImage] = useState('');
     const [price, setPrice] = useState(0);
+
+    const uploadImage = async (e) => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+        const res = await fetch('https://api.cloudinary.com/v1_1/jorch/image/upload', {method: 'POST', body: data});
+        const file = await res.json();
+        setImage(file.secure_url);
+        setLargeImage(file.eager[0].secure_url);
+    };
+
     return (
         <Mutation mutation={CREATE_ITEM_MUTATION} variables={{ title, description, price, image, largeImage }}>
             {(createItem, { loading, error}) => (
@@ -44,6 +56,17 @@ const CreateItem = () => {
                 }}>
                     <ErrorMessage error={error}/>
                     <fieldset disabled={loading} aria-busy={loading}>
+                        <label htmlFor='file'>
+                            Title
+                            <input
+                                type='file'
+                                id='file'
+                                name='file'
+                                placeholder='Upload an Image'
+                                required
+                                onChange={uploadImage}/>
+                            {image && <img src={image} width='200' alt='Upload Preview' />}
+                        </label>
                         <label htmlFor='title'>
                             Title
                             <input
