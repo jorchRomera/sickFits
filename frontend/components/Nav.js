f4386d1585a0ffb1ea8bb0a2b2ee5544680540d1
Jorch <jorge.a.romera@gmail.com>
2020-09-30 00:29:58 -0300

    current logged in user with middleware and render props

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from "./User";
 
 // noinspection HtmlUnknownTarget
 const Nav = () => (
     <NavStyles>
+        <User>{
+            ({ data}) => {
+                if (!data) return null;
+                if (!data.me) return null;
+                return <p>{data.me.name}</p>
+            }
+        }</User>
         <Link href='/items'>
             <a>Shop</a>
         </Link>
