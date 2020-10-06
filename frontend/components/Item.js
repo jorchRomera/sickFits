5a6c71bd625a6921b6303a61981b3967cbefe3cc
Jorch <jorge.a.romera@gmail.com>
2020-10-05 23:56:43 -0300

    displaying cart items and totals

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
 import DeleteItem from "./DeleteItem";
+import AddToCart from "./AddToCart";
 
 const Item = ({ item }) => {
     return (
@@ -28,7 +29,7 @@ const Item = ({ item }) => {
                 }}>
                     <a>Edit</a>
                 </Link>
-                <button>Add To Cart</button>
+                <AddToCart id={item.id} />
                 <DeleteItem id={item.id}>Delete This Item</DeleteItem>
             </div>
         </ItemStyles>
